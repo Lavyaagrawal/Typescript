8248d5ced7625edf3a45bde59500ce4a14a6101f
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-04-11 16:56:47 +0530

    learned about number boolean and type inference

--- variableMe.js
+++ variableMe.js
@@ -1,5 +1,9 @@
-"use strict";
 //typscript syntax for declaring variable
 //let variable name: datatype=value
 let greetings = "Hello lavya";
 console.log(greetings);
+//number
+let userId = 334466;
+//boolean
+let isLoggedIn = false;
+export {};
--- variableMe.ts
+++ variableMe.ts
@@ -3,4 +3,15 @@
 let greetings: string = "Hello lavya";
 console.log(greetings)
 
+
+//number
+let userId: number = 334466;
+
+//boolean
+let isLoggedIn: boolean=false;
+//🧠 What is Type Inference?
+// 👉 Type inference means:
+// TypeScript automatically figures out the type of a variable without you explicitly writing it.
+
+
 export {}
