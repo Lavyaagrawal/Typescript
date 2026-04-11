fb7d7c7dd46b987d02de05ce5addbbf45c3cba12
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-04-11 16:44:48 +0530

    learned about syntax of typscript

---
+++ variableMe.js
@@ -0,0 +1,5 @@
+"use strict";
+//typscript syntax for declaring variable
+//let variable name: datatype=value
+let greetings = "Hello lavya";
+console.log(greetings);
---
+++ variableMe.ts
@@ -0,0 +1,6 @@
+//typscript syntax for declaring variable
+//let variable name: datatype=value
+let greetings: string = "Hello lavya";
+console.log(greetings)
+
+export {}
