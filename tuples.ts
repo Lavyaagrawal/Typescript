88b0cb1e1aeba85f3b8e096d65e016a4b159d659
Lavya Agrawal <lavyaagrawal123@gmail.com>
2026-04-18 20:46:16 +0530

    new

---
+++ tuples.ts
@@ -0,0 +1,15 @@
+//const user1: (string|number)[]=[1,"hc"]
+//tuples
+let user1: [string,number,boolean]
+//the order of data matter based on datatypes
+user1=["hc",131,true]
+
+let rgb:[number,number,number]=[225,123,112]
+
+type User=[number,string]
+
+const newUser: User=[112,"example@google.com"]
+
+
+newUser[1]="hc.com";
+newUser.push(121,"lavya")
